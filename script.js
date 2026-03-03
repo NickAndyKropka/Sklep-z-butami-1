ba0acccd9dd1634330676ee09fe5244ff5e4153b
Kamcio <kamciosiamsio@gmail.com>
2026-03-03 11:48:35 +0100

    Nagłówki w podstronach

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.onload = function()
     filtr = document.getElementById("filtr");
     filtr.addEventListener("change", Filtrowanie())
 
-    function Filtrowanie() {
-        Filtruj
-    }
+    // function Filtrowanie() {
+    //     Filtruj
+    // }
 }
