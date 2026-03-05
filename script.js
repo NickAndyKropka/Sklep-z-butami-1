b12124a8dd100c32d0139f833b412135576e9b75
Kamcio <kamciosiamsio@gmail.com>
2026-03-05 13:02:20 +0100

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
         // Get references to input and list
         const filtr = document.getElementById('filtr');
         const lista = document.querySelectorAll('.lista li');
+        const marka = document.getElementById('marka');
 
         // Listen for input changes
         filtr.addEventListener('input', function () {
-        const filterText = this.value.trim().toLowerCase();
+        const filtrujtekst = this.value.trim().toLowerCase();
+
+        marka.addEventListener('input', function () {
+            const filtrujmarki = this.value;
+        })
 
         // Loop through list items and toggle visibility
         lista.forEach(item => {
             const text = item.textContent.toLowerCase();
-            item.classList.toggle('hidden', !text.includes(filterText));
+            item.classList.toggle('hidden', !text.includes(filtrujtekst));
+
+        lista.forEach(item => {
+            const marka = item.textContent;
+            item.classList.toggle('hidden', !marka.includes(filtrujmarki));
+        })
         });
     });
 });
