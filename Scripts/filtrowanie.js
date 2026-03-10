a84c67324709ffdc1ec163d7e38a80f962c1d330
NickAndyKropka <mikolajandy123@gmail.com>
2026-03-10 12:59:26 +0100

    modified:   Scripts/filtrowanie.js 	modified:   index.html

--- Scripts/filtrowanie.js
+++ Scripts/filtrowanie.js
@@ -3,9 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
         const filtr = document.getElementById('filtr');
         const lista = document.querySelectorAll('.lista li');
         const nazwa = document.querySelectorAll('.nazwa');
-        const marka = document.getElementById('marka');
-        const kat = document.getElementById('kat');
-        const rodz = document.getElementById('rodz');
+        // const marka = document.getElementById('marka');
+        // const kat = document.getElementById('kat');
+        // const rodz = document.getElementById('rodz');
+        const filtr1 = document.querySelectorAll('.filtr');
 
         // Listen for input changes
         filtr.addEventListener('input', function () 
@@ -20,32 +21,73 @@ document.addEventListener("DOMContentLoaded", function () {
                 li.classList.toggle('hidden', !text.includes(filtrujtekst));
             });
         });
-        marka.addEventListener('input', function () 
-        {
-            const filtrujmarki = this.value;
-            lista.forEach(item => 
+        filtr1.forEach(filtr =>
+            filtr.addEventListener('input', function ()
             {
-                const marka = item.textContent;
-                item.classList.toggle('hidden', !marka.includes(filtrujmarki));
-            });
-        });
-        kat.addEventListener('input', function() 
-        {
-            const filtrujkat = this.value;
-            lista.forEach(item => 
-            {
-                const kat = item.textContent;
-                item.classList.toggle('hidden', !kat.includes(filtrujkat));
-            });
-        });
-        rodz.addEventListener('input', function() 
-        {
-            const filtrujrodz = this.value;
-            lista.forEach(item => 
-            {
-                const rodz = item.textContent;
-                item.classList.toggle('hidden', !rodz.includes(filtrujrodz));
-            });
-        });
+                const filtrujtekst = this.value;
+                lista.forEach(item =>
+                {
+                    const text = item.textContent;
+                    if(text.includes(filtrujtekst))
+                    {
+                        item.classList.remove('hidden');
+                    }
+                    else
+                    {
+                        item.classList.add('hidden');
+                    }
+                });
+            }));
+        
+
+        // marka.addEventListener('input', function () 
+        // {
+        //     const filtrujmarki = this.value;
+        //     lista.forEach(item => 
+        //     {
+        //         const marka = item.textContent;
+        //         if(marka.includes(filtrujmarki))
+        //         {
+        //             item.classList.remove('hidden');
+        //         }
+        //         else
+        //         {
+        //             item.classList.add('hidden');
+        //         }
+        //     });
+        // });
+        // kat.addEventListener('input', function() 
+        // {
+        //     const filtrujkat = this.value;
+        //     lista.forEach(item => 
+        //     {
+        //         const kat = item.textContent;
+        //         if(kat.includes(filtrujkat))
+        //         {
+        //             item.classList.remove('hidden');
+        //         }
+        //         else
+        //         {
+        //             item.classList.add('hidden');
+        //         }
+
+        //     });
+        // });
+        // rodz.addEventListener('input', function() 
+        // {
+        //     const filtrujrodz = this.value;
+        //     lista.forEach(item => 
+        //     {
+        //         const rodz = item.textContent;
+        //         if(rodz.includes(filtrujrodz))
+        //         {
+        //             item.classList.remove('hidden');
+        //         }
+        //         else
+        //         {
+        //             item.classList.add('hidden');
+        //         }
+        //     });
+        // });
 
 });
