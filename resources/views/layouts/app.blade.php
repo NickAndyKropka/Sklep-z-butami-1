c8ad71cbd955880d10e9b38f1a1cde7f8e431e8f
Kamcio715 <kamciosiamsio@gmail.com>
2026-03-18 10:12:06 +0100

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sklep z butami</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/naglowek.css') }}"> 
+    <script src="{{ asset('js/filtrowanie.js') }}"></script>
     </head>
 <body>
     <header class="topbar py-3">
@@ -36,7 +31,7 @@
                     </form>
                 @else
                     <a class="btn btn-soft btn-sm" href="{{ route('login') }}">Logowanie</a>
-                    <a class="btn btn-dark btn-sm" href="{{ route('register') }}" style="background-color: white; color: black;">Rejestracja</a>
+                    <a class="btn btn-dark btn-sm" href="{{ route('register') }}">Rejestracja</a>
                 @endauth
             </div>
         </div>
@@ -52,5 +47,6 @@
 
             @yield('content')
         </div>
-    </body>
+    </main>
+</body>
 </html>
